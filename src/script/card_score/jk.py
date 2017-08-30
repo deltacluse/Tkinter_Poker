e3aec999cf78ab903b573449256e43e2f05a5748
deltacluse <deltacluse@naver.com>
2017-08-30 21:30:12 +0900

    start like this
    script 폴더 밑에 맡은 분야 이름으로 된 폴더를 만들어주세요~

---
+++ src/script/card_score/jk.py
@@ -0,0 +1,47 @@
+def checkScore(pattern, number) :  # pattern-카드 무늬 배열, number-카드 숫자 배열
+
+    score = 0
+
+    if() :  # royal straight flush
+        score += 1200
+
+    elif() :  # back straight flush
+        score += 1100
+
+    elif() :  # straight flush
+        score += 1000
+
+    elif() :  # four card
+        score += 900
+
+    elif() :  # full house
+        score += 800
+
+    elif() :  # flush
+        score += 700
+
+    elif() :  # mountain
+        score += 600
+
+    elif() :  # back straight
+        score += 500
+
+    elif() :  # straight
+        score += 400
+
+    elif() :  # triple
+        score += 300
+
+    elif() :  # two pair
+        score += 200
+
+    elif() :  # one pair
+        score += 100
+
+    elif() :  # top
+        # don't plus score
+        if() :
+
+class check :
+    def patternCheck(self) :
+        
