03f0b7b6833c8c65a340806ba334902cccbcae56
deltacluse <deltacluse@naver.com>
2017-09-04 21:00:21 +0900

    commit jk
    족보 파일 수정

--- src/script/card_score/jk.py
+++ src/script/card_score/jk.py
@@ -1,47 +1,93 @@
-def checkScore(pattern, number) :  # pattern-카드 무늬 배열, number-카드 숫자 배열
+class check :
+    def __init__(self, pattern, number) :  # pattern-카드 무늬 배열, number-카드 숫자 배열
+        self.pattern = pattern
+        self.number = number
+        self.patternCheck()
+        self.numberCheck()
 
-    score = 0
+    def patternCheck(self) :
+        self.patternCount = [0, 0, 0, 0]
+        for i in self.pattern :
+            if(i=="S") : self.patternCount[0] += 1
+            elif(i=="D") : self.patternCount[1] += 1
+            elif(i=="H") : self.patternCount[2] += 1
+            elif(i=="C") : self.patternCount[3] += 1
 
-    if() :  # royal straight flush
-        score += 1200
+        self.patternMax = 0
+        for i in range(4) :
+            if(self.patternMax<self.patternCount[i]) :
+                self.patternMax = self.patternCount[i]
+#            print(self.patternCount[i])
 
-    elif() :  # back straight flush
-        score += 1100
+    def numberCheck(self) :
 
-    elif() :  # straight flush
-        score += 1000
+        self.numberCount = [0]
+        for i in range(13) :
+            self.numberCount.append(0)
+        for i in self.number :
+            self.numberCount[i] += 1
 
-    elif() :  # four card
-        score += 900
+        self.countNumber = [0, 0, 0, 0, 0]
+        for i in self.numberCount :
+            self.countNumber[i] += 1
 
-    elif() :  # full house
-        score += 800
+        self.numberMax = 0
+        for i in range(14) :
+            if(self.numberMax<self.numberCount[i]) :
+                self.numberMax = self.numberCount[i]
+                self.numberMaxCount = i
 
-    elif() :  # flush
-        score += 700
+#        print(self.numberCount[i])
 
-    elif() :  # mountain
-        score += 600
 
-    elif() :  # back straight
-        score += 500
+    def checkScore(self):
 
-    elif() :  # straight
-        score += 400
+        score = 0
 
-    elif() :  # triple
-        score += 300
+        if ():  # royal straight flush
+            score += 1200
 
-    elif() :  # two pair
-        score += 200
+        elif ():  # back straight flush
+            score += 1100
 
-    elif() :  # one pair
-        score += 100
+        elif ():  # straight flush
+            score += 1000
 
-    elif() :  pass
-        # top
-        # don't plus score
+        elif (self.countNumber[4] == 1):  # four card
+            score += 900
 
-#class check :
-    #def patternCheck(self) :
-        
+        elif (self.countNumber[3] == 1 and self.countNumber[2] >= 1):  # full house
+            score += 800
+
+        elif (self.patternMax == 5):  # flush
+            score += 700
+
+        elif ():  # mountain
+            score += 600
+
+        elif ():  # back straight
+            score += 500
+
+        elif ():  # straight
+            score += 400
+
+        elif (self.countNumber[3] >= 1):  # triple
+            score += 300
+
+        elif (self.countNumber[2] >= 2):  # two pair
+            score += 200
+
+        elif (self.countNumber[2] == 1):  # one pair
+            score += 100
+
+        else :  # top
+            # don't plus score
+            pass
+
+        print(score)
+
+
+testPattern = ["S", "D", "D", "S", "H", "C", "C"]
+testNumber = [1, 3, 7, 3, 7, 9, 10]
+test = check(testPattern, testNumber)
+test.checkScore()
