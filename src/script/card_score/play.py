795194f4c031e52b86f2ffb32b6d02ff2dff54a5
Blank <blank6124@gmail.com>
2017-09-04 12:40:31 +0900

    Update play.py

--- src/script/card_score/play.py
+++ src/script/card_score/play.py
@@ -1,34 +1,28 @@
 import random
 
 #덱 셔플
-card = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13",
-        "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13",
-        "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13",
-        "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13"]
-
-card_list = card
-random.shuffle(card_list)
+class card :
+	def __init__(self) :
+		self.original_card = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13",
+								"D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13",
+								"H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13",
+								"C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13"]
+	
+	#게임 시작
+	def game_start(self) :
+		playing_deck = self.original_card
+		random.shuffle(playing_deck)
+		return playing_deck
+	
+	#카드 분배 
+	def deal(self) :
+		for i in range(3) :
+			for j in range(4) :
+				playing_card[(idx + j) % 4].append(card_list[0])
+				del card_list[0]
+		return playing_card
+	
 
 #player 순서 결정
 player = ["player", "computer1", "computer2", "computer3"]
-
 idx = 0
-
-print(player[idx])
-
-#카드 분배
-playing_card = [[], [], [], []]
-def deal(playing_card) :
-    for i in range(4) :
-        for j in range(4) :
-            playing_card[(idx + j) % 4].append(card_list[0])
-            del card_list[0]
-        
-
-
-
-
-# 카드 버리기
-def card_drop(drop_card) :
-        for i in range(4) :
-                playing_card[(idx + j) % 4].remove(drop_card[j])
