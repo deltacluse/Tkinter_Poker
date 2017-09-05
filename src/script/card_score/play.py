e47c6b226715b8524b8fb645a11e6e07252d6c6d
Blank <blank6124@gmail.com>
2017-09-06 08:22:21 +0900

    0906

--- src/script/card_score/play.py
+++ src/script/card_score/play.py
@@ -3,10 +3,10 @@ import random
 #덱 셔플
 class card :
 	def __init__(self) :
-		self.original_card = ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13",
-								"D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13",
-								"H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13",
-								"C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13"]
+		self.original_card = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13",
+								"D01", "D02", "D03", "D04", "D05", "D06", "D07", "D08", "D09", "D10", "D11", "D12", "D13",
+								"H01", "H02", "H03", "H04", "H05", "H06", "H07", "H08", "H09", "H10", "H11", "H12", "H13",
+								"C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12", "C13"]
 	
 	#게임 시작
 	def game_start(self) :
@@ -16,7 +16,8 @@ class card :
 	
 class gameStart :
 	def __init__(self) :
-	
+		player = ["player", "computer1", "computer2", "computer3"]
+		
 	#순서 결정
 	def order(self, winner) :
 		if winner == 0 :
@@ -24,13 +25,17 @@ class gameStart :
 		else :
 			return winner
 	
-	#카드 분배 
-	def deal(self, idx) :
+	#카드 처음 분배 
+	def deal_first(self, idx) :
 		for i in range(3) :
 			for j in range(4) :
 				playing_card[(idx + j) % 4].append(card_list[0])
 				del card_list[0]
 		return playing_card
 	
-#player 순서 결정
-player = ["player", "computer1", "computer2", "computer3"]
+	# 분배
+	def deal(self, idx) :
+		for i in range(len(playing_card)) :
+			playing_card[(idx + j) % 4].append(card_list[0])
+			del card_list[0]
+		return playing_card
