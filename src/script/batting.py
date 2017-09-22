eddb94de5cc288b9bb19dc3b7cd5564b29ece590
deltacluse <deltacluse@naver.com>
2017-09-22 12:41:08 +0900

    modify batting.py
    턴 계산을 클래스 박으로 빼고 클래스 메소드들 정리

--- src/script/batting.py
+++ src/script/batting.py
@@ -1,59 +1,37 @@
 #-*- coding: utf-8 -*-
-#되도록 혼자 해보고 싶지만 어떻게 해야될지 모르겠으니 마구잡이로 시작합니다.
 
 class batting:
-    def __init__(self, hvmoney, btmoney):
-        self.btmoney = btmoney
-        self.hvmoney = hvmoney
-        self.tablemoney = 0
-        self.beformoney = 0
-        self.turn = 1
-        self.raizcount = 0
-        self.callcount = 0
+    def __init__(self, hvMoney):
+        self.hvMoney = hvMoney
+        self.update(0, 0)
 
     # raise
-    def raiz(self, time):
-        btmoney = tablemoney * (time - 1)
-        if hvmoney < btmoney:
-            btmoney = hvmoney
-        beformoney = btmoney
-        tablemoney += btmoney
-        raizcount = 1
-        callcount = 0
+    def raiz(self, times):
+        self.btMoney = self.tableMoney * (times - 1)
+        if self.hvMoney < self.btMoney:
+            self.btMoney = self.hvMoney
+        self.beforeMoney = self.btMoney
+        self.tableMoney += self.btMoney
 
     # Call
-    def Call(self, say, beformon, hvmoney, btmoney, turn, callcount, user, finishscen):
-        if hvmoney>=beformon:
+    def call(self, finishscen):
+        if self.hvMoney >= self.beformon:
+            self.btMoney = self.beformon
+        else:
+            self.btMoney = self.hvMoney
+        self.hvMoney -= self.btMoney
+        self.tableMoney += self.btMoney
 
-            btmoney = beformon
-            hvmoney = hvmoney - btmoney
+    def update(self, beforeMoney, tableMoney):
+        self.beforeMoney = beforeMoney
+        self.tableMoney = tableMoney
 
-            self.tablemoney += btmoney
-            self.raizcount = 0
-            self.callcount +=1
-            self.theend(raizcount, callcount, user, finishscen)
+turn = 0
+raizCount = 0
+callCount = 0
 
-        elif hvmoney<beformon:
-            self.allin(beformon, hvmoney, self.tablemoney)
+def turnCheck(playerList):
+    if(raizCount == 1 and callCount == len(playerList)-1):
 
-    #check
-    def check(self, beformon, btmoney, hvmoney, turn):
-        if hvmoney >=beformon and turn==1:
-            btmoney = 0
-            hvmoney = hvmoney - beformon
-        elif turn !=1:
-            pass
 
-        elif hvmoney<beformon:
-            self.allin()
-
-    #end
-    def theend(raiz, call, user, finishscen):
-        if raiz == 1 and call == user:
-            return finishscen
-
-    #올인
-    def allin(tablemoney, hymoney, btmoney):
-        tablemoney = tablemoney + hvmoney
-        btmoney = hvmoney
-        hvmoney = 0
+player = ['Kim', 'Ai1', 'Ai2', 'Ai3']
