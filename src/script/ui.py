8f08a2f657aa7e45de1669d00a13aa1d999178ef
deltacluse <deltacluse@naver.com>
2017-10-30 23:18:08 +0900

    GUI fix
    배경색 맞추기
    카드들 이미지 등록

--- src/script/ui.py
+++ src/script/ui.py
@@ -2,31 +2,31 @@ from tkinter import *
 
 root=Tk()
 
-FullWidth = root.winfo_screenwidth() - 16
-FullHeight = root.winfo_screenheight() - 100 
+#FullWidth = root.winfo_screenwidth() - 16
+#FullHeight = root.winfo_screenheight() - 100 
 
-#FullWidth = 1366
-#FullHeight = 768
+FullWidth = 1366
+FullHeight = 768
 
 root.title("PokerGame")
 root.geometry("{0}x{1}+0+20".format(FullWidth, FullHeight))
 root.configure(background ='#22741C')
-#root.resizable(False, False)
+root.resizable(False, False)
 
 setHeight = {
     'otherFrame' : FullHeight * 0.4,
-    'battingFrame' : FullHeight * 0.2,
-    'myFrame' : FullHeight * 0.4
+    'battingFrame' : FullHeight * 0.3,
+    'myFrame' : FullHeight * 0.3
 }
 
 #OtherPlayer Frame
-otherFrame = Frame(root, background='white', height=setHeight['otherFrame'], width=FullWidth)
+otherFrame = Frame(root, background='#22741C', height=setHeight['otherFrame'], width=FullWidth)
 otherFrame.place(x = 0, y = 0)
 #Batting Frame
-battingFrame = Frame(root, background = 'black', height=setHeight['battingFrame'], width=FullWidth)
+battingFrame = Frame(root, background = '#22741C', height=setHeight['battingFrame'], width=FullWidth)
 battingFrame.place(x = 0, y = setHeight['otherFrame'])
 #My Frame
-myFrame = Frame(root, background = 'green', height=setHeight['myFrame'], width=FullWidth)
+myFrame = Frame(root, background = '#22741C', height=setHeight['myFrame'], width=FullWidth)
 myFrame.place(x = 0, y = setHeight['otherFrame'] + setHeight['battingFrame'])
 
 #Other Player Grid
@@ -36,7 +36,7 @@ myFrame.place(x = 0, y = setHeight['otherFrame'] + setHeight['battingFrame'])
 otherCell = [] #OtherPlayer Cell Group
 otherCellSub = [[],[],[]] #OtherPlayer Cell Card, Batting, Money
 for i in range(3):
-    player = Frame(otherFrame, background = 'white')
+    player = Frame(otherFrame, background = '#22741C')
     player.grid(row=0, column=i, padx = FullWidth*0.4/6, pady = setHeight['otherFrame']/7/4)
     otherCell.append(player)
     for j in range(3) :
@@ -52,11 +52,11 @@ battedMoney.pack(padx = FullWidth * 0.4, pady = setHeight['battingFrame']*0.2)
 text_battedMoney.set("tablemoney")
 
 #My Grid
-moneyFrame = Frame(myFrame, background = 'blue', height = setHeight['myFrame'], width = FullWidth * 0.2)
+moneyFrame = Frame(myFrame, background = '#22741C', height = setHeight['myFrame'], width = FullWidth * 0.2)
 moneyFrame.grid(row = 0, column = 0)
-CardFrame = Frame(myFrame, background = 'white', height = setHeight['myFrame'], width = FullWidth * 0.6)
+CardFrame = Frame(myFrame, background = '#22741C', height = setHeight['myFrame'], width = FullWidth * 0.6)
 CardFrame.grid(row = 0, column = 1)
-ButtonFrame = Frame(myFrame, background = 'blue', height = setHeight['myFrame'], width = FullWidth * 0.2)
+ButtonFrame = Frame(myFrame, background = '#22741C', height = setHeight['myFrame'], width = FullWidth * 0.2)
 ButtonFrame.grid(row = 0, column = 2)
 
 #My Money Label
@@ -66,11 +66,12 @@ battedMoney.pack(pady = setHeight['myFrame'] * 0.2)
 text_battingMoney.set("mymoney")
 
 #My Cards
+img_battedMoney = []
 for i in range(7):
-    img_battedMoney = StringVar() #여기에 카드 이미지 링크
-    card = Label(CardFrame, background = 'red', height = int(setHeight['myFrame'] / 15), width = int(setHeight['myFrame'] / 8 * 0.75), textvariable = img_battedMoney, foreground = 'white', relief = SOLID)
-    card.place(x = i * (FullWidth * 0.6 - FullHeight * 0.4 * 0.75)/5.83, y = 0)
-    img_battedMoney.set("card " + format(i + 1))
+    img_battedMoney.append(PhotoImage(file='..\image\c{0} (1).png'.format(i+1)))
+    img_battedMoney[i] = img_battedMoney[i].zoom(int(setHeight['myFrame'] * 0.75 / 65), int(setHeight['myFrame'] / 92))
+    card = Label(CardFrame, background = 'white', height = setHeight['myFrame'] * 0.8, width = setHeight['myFrame'] * 0.75 * 0.8, image = img_battedMoney[i])
+    card.place(x = i * (FullWidth * 0.6 / 8) + (FullWidth * 0.3 * 0.25 - setHeight['myFrame'] * 0.75 * 0.4), y = setHeight['myFrame'] * 0.1)
     
 
 #Batting Button
